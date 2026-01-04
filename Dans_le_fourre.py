c485cc4635dad96e7a2f0525d1bfaa1cb6be3bf1
abdessalem16 <abdessalemgitlahouel@gmail.com>
2026-01-04 13:24:33 +0100

    Add dans le fourré

---
+++ Dans_le_fourre.py
@@ -0,0 +1,17 @@
+# Dans le fourré
+
+# Sur l’étroit chemin qui mène à la montagne, vous parvenez à une énorme porte qui vous empeche de passer. Les villageois vous ont prévenu que la clé de cette porte demeure dans le fourré qui se trouve juste à cote. Celui-ci est organisé selon des cases avec des pièges et des buissons, dont on vous a fourni le plan.
+# Cependant, vous n’etes pas sur que le plan soit tout à faut exact et vous redoutez les pièges du fourré. Vous décidez donc d’envoyer votre robot chercher la clé pour vous 
+# Ce que doit faire votre programme :
+# Votre programme doit diriger votre robot dans la grille suivante :
+# Le robot se trouve initialement à l’entrée du fourré et doit atteindre la case où se trouve la clé les cases où se trouve des buissons infranchissables ni celles qui contiennent piège.
+# Vous n’avez pas besoin de programmer le chemin retour.
+# *********************************
+from robot import * 
+for loop  in range(3):
+	haut()
+for loop in range(2):
+	droite()
+for loop I in range():
+	bas()
+droite()
