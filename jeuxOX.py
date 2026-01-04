27eb51fbaff07b57ffba8ae7b93cb320802b391b
abdessalem16 <abdessalemgitlahouel@gmail.com>
2026-01-04 14:25:34 +0100

    Add jeuxOX

---
+++ jeuxOX.py
@@ -0,0 +1,12 @@
+# Un deux jeux les plus populaires sur Algoréa est une variante du jeu de dames que vous connaissez bien.
+# Le jeu utilise un plateau similaire à un damier mais de taille 40 par 40. 
+# Les villageois vous proposent de jouer contre eux, mais malheureusement ils n’arrivent pas
+#  A retrouver leur plateau de jeu.
+# Plutôt que de passer des heures à le chercher, vous leur proposer de leur en imprimer un nouveau
+for OXOX in range(20):
+    for OX in range(20):
+        print("OX",end="")
+    print()
+    for XO in range(20):
+        print("XO",end="")
+    print()
