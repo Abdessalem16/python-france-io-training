cbfb7debe21a91a8f15b830606386247959ec4a1
abdessalem16 <abdessalemgitlahouel@gmail.com>
2026-01-05 20:15:01 +0100

    ajout invasion de batraciens

---
+++ invasion_de_batraciens.py
@@ -0,0 +1,14 @@
+# invasion de batraciens
+# l’automne ayant été très pluvieux , les Bufo Algo, une espèce locale de crapauds, se sont reproduits en grand nombre, et les habitants ont constaté que leur nombre doublait chaque semaine ! Leurs prédateurs naturels, les couleuvres(un type de serpent),
+# sont complètement dépassés !
+# Avoir trop de crapauds est très génenat (on ne plus dormir) !
+# Les villageois décident donc d’élever un grand nombre de couleuvres avec
+# Lesquelles ils pourront contrôler le nombre de crapauds.
+# Il leur tout d’abord estimer le nombre de crapauds qu’il y aura au cours des semaines qui viennent. Ils demandent votre aide.
+# Ce que doit faire votre programme :
+# Sachant qu’il y a actuellement 1337 crapauds et que leur nombre double chaque semaine, votre programme devra afficher le nombre de crapauds qu’il y aura après la 12 e semaine.
+# Important : vous devez utiliser une boucle pour calculer le nombre de crapauds.
+NombreCrapauds=1337
+for loop in range(12):
+    NombreCrapauds=NombreCrapauds*2 
+print(NombreCrapauds)
