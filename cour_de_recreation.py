6862ba0ec3583f454e0861d90b9505b077b0299c
abdessalem16 <abdessalemgitlahouel@gmail.com>
2026-01-05 19:23:45 +0100

    ajout cour de récréation

---
+++ cour_de_recreation.py
@@ -0,0 +1,13 @@
+# cour de récréation
+# la cour carré a été mesurée avec quatre bâtons de longueurs respectives 17 m, 7m, 5m et 2m.
+# la longueur du coté de la cour est égale à 5 fois le premier bâton plus 2 fois le second plus
+# 1 fois le troisième plus 2 fois le quatrième.
+# Votre programme doit afficher deux lignes : 
+# La première doit contenir la surface de la cour, et la seconde ligne doit contenir son périmètre.
+# Les résultats doivent être exprimés en mètres carrés et en mètres, respectivement, mais vous en devez pas afficher l’unité après la valeur numérique.
+# Important : dans votre programme, commencez par calculer la longueur du coté de la cour et l’enregistrer dans une variable.
+batons =(17*5)+(7*2)+(5*1)+(2*2)
+surface=batons*batons
+perimatre=batons*4
+print(surface)
+print(perimetre)
