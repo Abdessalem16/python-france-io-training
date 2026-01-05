e29abe57b2433e00d7956d0d2afc3da8d46750e9
abdessalem16 <abdessalemgitlahouel@gmail.com>
2026-01-05 20:06:33 +0100

    ajout une partie de cache cache

---
+++ une_partie_de_cache_cache.py
@@ -0,0 +1,17 @@
+# une partie de cache-cache
+# à la récréation, vous croisez un groupe d’enfants qui vous proposent de participer à leur jeu.
+# Vous acceptez de bon cœur et voilà votre robot engagé dans une partie de cache-cache !
+# Ce que doit faire votre programme :
+# Le robot devra compter jusqu’à 100 , c’est-à-dire afficher les entiers de 1 à 100, un par ligne, 
+# Et ensuite afficher « j’arrive ! » s’ik ne devait compter que jusqu’à 3 au lieu de 100, votre robot devrait afficher :
+# devrait afficher :
+# 1
+# 2
+# 3
+# J’arrive ! 
+# Important : votre programme ne doit pas faire plus d’une quinzaine de lignes.
+# Code :
+for loop in range(1,101):
+    print(loop)
+print("J'arrive !")
+
