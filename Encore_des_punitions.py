2970edf2afb1c8774355cd229268a214f22f908f
abdessalem16 <abdessalemgitlahouel@gmail.com>
2026-01-06 20:05:40 +0100

    Ajout Encore des punitions

---
+++ Encore_des_punitions.py
@@ -0,0 +1,13 @@
+# Encore des punitions 
+# Les élèves du village ayant très souvent des punition , 
+# Ils aimeraient pouvoir  venir les imprimer facilement sans 
+# Que vous soyez là à chaque fois. 
+# Il leur suffirait d’indiquer le nombre de lignes voulu et le robot 
+# Imprimerait tout seul la punition !
+# Ce que doit faire votre programme :
+# Votre programme doit lire un entier, le nombre de lignes souhaité, et écrira
+# Autant de fois que demandé la phrasé « je dois suivre en cours »
+# Code :
+Répeter = int(input())
+for i in range(Répeter):
+    print("je dois suibre en cours")
