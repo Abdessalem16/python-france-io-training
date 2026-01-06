60b4e91d19930d130dcc843d8455feff51231c27
abdessalem16 <abdessalemgitlahouel@gmail.com>
2026-01-06 20:02:36 +0100

    jout âge des petits-enfants

---
+++ age_des_petits_enfants.py
@@ -0,0 +1,14 @@
+#                âge des petits-enfants
+# Earane, la doyenne du village, a souhaité apprendre à programme
+# afin de pouvoir ensuite transmettre ce savoir à ses nombreux petits-enfants.
+# Cependant, en dépit des quelques cours que vous lui avez données, elle n’arrive pas 
+# A terminer son premier programme. 
+# Pouvez-vous l’aider à corriger ses erreurs ?
+# Ce que doit faire votre programme :
+# Votre programme doit être une version corrigée du programme ci-dessous, 
+# Sachant qu’il vous faut changer le moins de choses possible.
+# Code :
+âgeCadet = int(input())
+âgeAîné  = int(input())
+différence =âgeAîné - âgeCadet
+print(différence)
