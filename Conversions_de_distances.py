260648bec548619c9f93516888e64e07cee5da97
abdessalem16 <abdessalemgitlahouel@gmail.com>
2026-08-17 13:39:25 +0200

    Conversions de distances

---
+++ Conversions_de_distances.py
@@ -0,0 +1,17 @@
+Vous décidez de partir pour quelques jours de randonnée à la montagne. Le problème est que toutes les distances indiquées sur les panneaux ne le sont pas en kilomètres mais en lieues. Vous aimeriez être en mesure de faire les conversions automatiquement.
+
+Ce que doit faire votre programme :
+Écrivez un programme qui lit un nombre décimal (un nombre à virgule) représentant un nombre de lieues et affiche le nombre de kilomètres correspondant. Un kilomètre vaut exactement 0.707 lieues.
+
+Exemple
+entrée :
+
+10.5
+sortie :
+
+14.8514851485
+___________________________
+def main():
+    lieues=float(input())
+    print(lieues/0.707)
+main()    
