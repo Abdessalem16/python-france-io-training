7ef749d855c9b1eb7f94987bcf4647e9ed3fe70b
abdessalem16 <abdessalemgitlahouel@gmail.com>
2026-09-02 18:43:04 +0200

    Analyse d'une langue

---
+++ Analyse_d_une_langue.py
@@ -0,0 +1,42 @@
+			Analyse d’une langue
+
+
+Au cours des siècles une langue évolue, et non seulement les mots apparaissent ou disparaissent, mais certaines lettres deviennent plus utilisées ou au contraire moins utilisées.
+
+Afin de pouvoir rapidement analyser de nombreux textes, on souhaite mettre au point un programme calculant combien de fois une lettre donnée est présente au sein d’un texte.
+
+Contraintes
+Chaque ligne de texte contient au plus 1000 caractères.
+
+Entrée
+Sur la première ligne, la lettre majuscule dont on doit chercher le nombre d’apparition dans le texte.
+
+Sur la seconde ligne, un entier nbLignes le nombre de lignes du texte.
+
+Sur les nbLignes lignes suivantes, le texte, ne contenant aucune lettre minuscule.
+
+Sortie
+Un seul entier, le nombre d’apparitions de la lettre au sein du texte.
+
+Exemple
+entrée :
+
+E
+2
+JE VOUS REMECTZ A LA GRANDE CHRONICQUE PANTAGRUELINE 
+RECONGNOISTRE LA GENEALOGIE ET ANTIQUITE DONT NOUS EST VENU GARGANTUA
+sortie :
+
+16
+________________________
+def main():
+    cherche =input()
+    nbLignes =int(input())
+    nombreE=0
+    for i in range (nbLignes):
+        texte=input()
+        for i in texte:
+            if i==cherche:
+                nombreE+=1
+    print(nombreE)            
+main()
