b49999e2da6221305c14ada63db5a6dd70c6fe4c
abdessalem16 <abdessalemgitlahouel@gmail.com>
2026-09-23 21:23:06 +0200

    Consonnes

--- Consonnes.py
+++ Consonnes.py
@@ -6,7 +6,7 @@ On utilisera bien entendu une boucle !
 Limites de temps et de mémoire (Python)
 Temps : 0,1 s sur une machine à 1 GHz.
 Mémoire : 32 000 ko.
-___________________
+________________________________________________________________________
 def main():
     for i in range(ord('a'),ord('z')+1):
         if not(chr(i) in ['a','e','i','o','u','y']):
